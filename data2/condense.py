a1e75aec9eef96341afbc0aa7aa8df203ea92572
atomousek <atomousek@gmail.com>
2020-06-30 10:02:09 +0200

    renaming

--- data2/condense.py
+++ data2/condense.py
@@ -2,47 +2,57 @@ import numpy as np
 import os
 
 
-model_orig_dir = "/home/filip/Documents/evaluating_flow/models/"
-model_new_dir = "/home/filip/Documents/evaluating_flow/models2/"
-test_dir = "/home/filip/Documents/evaluating_flow/data2/time_windows/"
+#model_orig_dir = "/home/filip/Documents/evaluating_flow/models/"
+#model_new_dir = "/home/filip/Documents/evaluating_flow/models2/"
+
+#test_dir = "/home/filip/Documents/evaluating_flow/data2/time_windows/"
+test_dir = "time_windows/"
 
 times = np.loadtxt("test_times_full.txt")
-models = ["cost_F_3_clusters_3_periodicities",
-            "model_daily_histogram",
-            "model_prophet",
-            "model_segment_means",
-            "model_weekly_histogram",
-            "occ_grid",
-            "published_cliffmap_model_pq",
-            "published_predictions_stef_euc_o2",
-            "published_ral_3_clusters_3_periodicities"]
+#models = ["cost_F_3_clusters_3_periodicities",
+#            "model_daily_histogram",
+#            "model_prophet",
+#            "model_segment_means",
+#            "model_weekly_histogram",
+#            "occ_grid",
+#            "published_cliffmap_model_pq",
+#            "published_predictions_stef_euc_o2",
+#            "published_ral_3_clusters_3_periodicities"]
+
+models = ['clifford_2_3_2_clusters_3_periodicities']
+model_orig_dir = "../"
+model_new_dir = "../models2/"
+
 
 # # condense models
-# for model in models:
-#     for i in range(len(times) // 2):
-#         t1 = times[2*i]
-#         t2 = times[2*i + 1]
+for model in models:
+ for i in range(len(times) // 2):
+     t1 = times[2*i]
+     t2 = times[2*i + 1]
 
-#         a1 = np.loadtxt(model_orig_dir + model + "/{}_model.txt".format(int(t1)))
-#         a2 = np.loadtxt(model_orig_dir + model + "/{}_model.txt".format(int(t2)))
+     #a1 = np.loadtxt(model_orig_dir + model + "/{}_model.txt".format(int(t1)))
+     #a2 = np.loadtxt(model_orig_dir + model + "/{}_model.txt".format(int(t2)))
+     a1 = np.load(model_orig_dir + model + "/{}_model.txt.npy".format(int(t1)))
+     a2 = np.load(model_orig_dir + model + "/{}_model.txt.npy".format(int(t2)))
 
-#         a1[:, 3] = a1[:, 3] + a2[:, 3]
-#         np.savetxt(model_new_dir + model + "/{}_model.txt".format(int(t1)), a1)
+     #a1[:, 3] = a1[:, 3] + a2[:, 3]
+     #np.savetxt(model_new_dir + model + "/{}_model.txt".format(int(t1)), a1)
+     np.savetxt(model_new_dir + model + "/{}_model.txt".format(int(t1)), a1)
 
-# condense testing data
-for i in range(len(times) // 2):
-    t1 = times[2*i]
-    t2 = times[2*i + 1]
-
-    with open(test_dir + "{}_test_data.txt".format(int(t1)), "a") as f1:
-        with open(test_dir + "{}_test_data.txt".format(int(t2)), "r") as f2:
-            if os.path.getsize(test_dir + "{}_test_data.txt".format(int(t2))):
-                if os.path.getsize(test_dir + "{}_test_data.txt".format(int(t1))):
-                    f1.write('\n')
-                    f1.write(f2.read())
-                else:
-                    f1.write(f2.read())
-
-    os.remove(test_dir + "{}_test_data.txt".format(int(t2)))
+## condense testing data
+#for i in range(len(times) // 2):
+#    t1 = times[2*i]
+#    t2 = times[2*i + 1]
+#
+#    with open(test_dir + "{}_test_data.txt".format(int(t1)), "a") as f1:
+#        with open(test_dir + "{}_test_data.txt".format(int(t2)), "r") as f2:
+#            if os.path.getsize(test_dir + "{}_test_data.txt".format(int(t2))):
+#                if os.path.getsize(test_dir + "{}_test_data.txt".format(int(t1))):
+#                    f1.write('\n')
+#                    f1.write(f2.read())
+#                else:
+#                    f1.write(f2.read())
+#
+#    os.remove(test_dir + "{}_test_data.txt".format(int(t2)))
 
 
--- src/main.py
+++ src/main.py
@@ -2,19 +2,24 @@ from test_lot_of_models import test_models, test_with_different_params
 
 
 def main():
-    speed = 1.
+    speed = 0.5
     radius_of_robot = 1.
-    weighted_encounters = False
+    weighted_encounters = True
 
-    models = ['published_ral_3_clusters_3_periodicities', 'published_cliffmap_model_pq',
-              'published_predictions_stef_euc_o2', 'model_daily_histogram',
-              'model_segment_means', 'model_weekly_histogram', 'model_prophet']
+    #models = ['published_ral_3_clusters_3_periodicities', 'published_cliffmap_model_pq',
+    #          'published_predictions_stef_euc_o2', 'model_daily_histogram',
+    #          'model_segment_means', 'model_weekly_histogram', 'model_prophet']
 
-    # test_models(models=models, speed=speed, radius_of_robot=radius_of_robot, weighted_encounters=weighted_encounters)
-    test_with_different_params('published_ral_3_clusters_3_periodicities',
-                               radii_of_robot=(1.,),
-                               speeds=(0.5, 1.0),
-                               weighted_encounters_opts=(False, ))
+    models = ['CLiFF', 'Means', 'occ_grid', 'Prophet', 'STeF', 'WHyTeS']
+
+    #test_models(models=models, speed=speed, radius_of_robot=radius_of_robot, weighted_encounters=weighted_encounters)
+    test_with_different_params('CLiFF',
+                               #radii_of_robot=(0.7, 1.0, 1.3, 1.6, 1.9),
+                               radii_of_robot=(1.0,),
+                               #speeds=(0.5, 0.75, 1.0, 1.25),
+                               speeds=(0.5,),
+                               weighted_encounters_opts=(True, False))
+                               #weighted_encounters_opts=(True,))
 
 
 if __name__ == '__main__':
