6cc7c1991878250e552fefe14933d26d93f9306f
Filip Kubis <kubisfil@fel.cvut.cz>
2020-06-30 16:14:36 +0200

    added requirements.txt and cleaned main.py

--- src/main.py
+++ src/main.py
@@ -1,26 +1,37 @@
 from test_lot_of_models import test_models, test_with_different_params
 
 
-def main():
+def test_many_models():
+    # choose parameters
     speed = 0.5
     radius_of_robot = 1.
     weighted_encounters = True
 
-    #models = ['published_ral_3_clusters_3_periodicities', 'published_cliffmap_model_pq',
-    #          'published_predictions_stef_euc_o2', 'model_daily_histogram',
-    #          'model_segment_means', 'model_weekly_histogram', 'model_prophet']
-
+    # choose models
     models = ['CLiFF', 'Histogram day', 'Histogram week', 'Means', 'occ_grid', 'Prophet', 'STeF', 'WHyTeS']
 
+    # run the tests
     test_models(models=models, speed=speed, radius_of_robot=radius_of_robot, weighted_encounters=weighted_encounters)
-    #test_with_different_params('CLiFF',
-    #                           #radii_of_robot=(0.7, 1.0, 1.3, 1.6, 1.9),
-    #                           radii_of_robot=(1.0,),
-    #                           #speeds=(0.5, 0.75, 1.0, 1.25),
-    #                           speeds=(0.5,),
-    #                           #weighted_encounters_opts=(True, False))
-    #                           weighted_encounters_opts=(True,))
+
+
+def test_different_parameters():
+    # choose model and parameters
+    model = 'CLiFF'
+    radii_of_robot = (0.7, 1.0, 1.3, 1.6, 1.9)
+    speeds = (0.5, 0.75, 1.0, 1.25)
+    weighted_encounters_opts = (True, False)
+
+    # run the tests
+    test_with_different_params(model=model,
+                               radii_of_robot=radii_of_robot,
+                               speeds=speeds,
+                               weighted_encounters_opts=weighted_encounters_opts)
 
 
 if __name__ == '__main__':
-    main()
+    """
+    Choose the test
+    Specify the desired parameters and models in the functions called below
+    """
+    test_many_models()
+    test_different_parameters()
--- src/test_lot_of_models.py
+++ src/test_lot_of_models.py
@@ -4,8 +4,8 @@ from summarize_new import summarize, plot_all
 import os
 
 default_models = ['published_ral_3_clusters_3_periodicities', 'published_cliffmap_model_pq',
-              'published_predictions_stef_euc_o2', 'model_daily_histogram',
-              'model_segment_means', 'model_weekly_histogram', 'model_prophet']
+                  'published_predictions_stef_euc_o2', 'model_daily_histogram',
+                  'model_segment_means', 'model_weekly_histogram', 'model_prophet']
 
 
 def test_models(models=default_models, speed=1, radius_of_robot=1, weighted_encounters=False):
@@ -37,12 +37,13 @@ def test_models(models=default_models, speed=1, radius_of_robot=1, weighted_enco
         for time in times:
             path_model = model_dir + '/' + str(model) + '/' + str(time) + '_model.txt'
             test_data_path = test_dir + str(time) + '_test_data.txt'
-            result = tester.test_model(path_model=path_model, path_data=test_data_path, testing_time=time, model_name=model, edges_of_cell=edges_of_cell, speed=speed, weighted_encounters=weighted_encounters)
+            result = tester.test_model(path_model=path_model, path_data=test_data_path, testing_time=time,
+                                       model_name=model, edges_of_cell=edges_of_cell, speed=speed,
+                                       weighted_encounters=weighted_encounters)
             with open(output_path, 'a') as file:
                 file.write(' '.join(str(value) for value in result) + '\n')
 
     for model in models:
-
         print('\n statistics of ' + model)
         output_path = result_dir + '/' + str(model) + '_output.txt'
         summarize(output_path)
@@ -50,8 +51,7 @@ def test_models(models=default_models, speed=1, radius_of_robot=1, weighted_enco
     plot_all([result_dir + '/' + str(model) + '_output.txt' for model in models], models, result_dir + '/models.pdf')
 
 
-def test_with_different_params(model, speeds=(1, ), radii_of_robot=(1, ), weighted_encounters_opts=(False, )):
-
+def test_with_different_params(model, speeds=(1,), radii_of_robot=(1,), weighted_encounters_opts=(False,)):
     print('testing  ' + model)
 
     result_dir = '../results/output'
@@ -79,16 +79,12 @@ def test_with_different_params(model, speeds=(1, ), radii_of_robot=(1, ), weight
                 tester = Tester(radius_of_robot=radius_of_robot)
                 times = np.loadtxt(times_path, dtype='int')
 
-                #print('speed: ' + str(speed))
-                #print('radius_of_robot: ' + str(radius_of_robot))
-                #print('weighted_encounters: ' + str(weighted_encounters))
-
                 output_path = result_dir + '/' + current_model + '_output.txt'
                 outputs.append(output_path)
 
                 if os.path.exists(output_path):
                     os.remove(output_path)
-                
+
                 for time in times:
                     path_model = model_dir + '/' + str(model) + '/' + str(time) + '_model.txt'
                     test_data_path = test_dir + str(time) + '_test_data.txt'
@@ -97,10 +93,10 @@ def test_with_different_params(model, speeds=(1, ), radii_of_robot=(1, ), weight
                                                weighted_encounters=weighted_encounters)
                     with open(output_path, 'a') as file:
                         file.write(' '.join(str(value) for value in result) + '\n')
-                
+
                 print('\n statistics of {} with speed={}m/s, r={}m and weighted encounters set to {}.'.format(
                     model, speed, radius_of_robot, weighted_encounters))
-                
+
                 summarize(output_path)
 
     plot_all(outputs, models, result_dir + '/' + model + '_params.pdf')
